19146a8cfd17459e64e2c2f1d39005796b585197
Josh Wang <winstar53@hotmail.com>
2015-08-17 19:49:52 +0800

    have a try

---
+++ src/com/eveow/leetcode/Case003.java
@@ -0,0 +1,12 @@
+package com.eveow.leetcode;
+
+/**
+ * Title: Case003.class<br>
+ * Description: <br>
+ * Copyright (c) 人和网版权所有 2015    <br>
+ * Create DateTime: 2015年08月17日 下午7:49 <br>
+ *
+ * @author Josh Wang
+ */
+public class Case003 {
+}
